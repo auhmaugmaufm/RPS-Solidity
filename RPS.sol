fb1d62ae36a84431738a4a2257aa48ace94f1a6d
auhmaugmaufm <supitcha.aq@gmail.com>
2025-03-08 22:42:18 +0700

    hash mai thong pom tong tum ngai

--- RPS.sol
+++ RPS.sol
@@ -8,12 +8,15 @@ import "./TimeUnit.sol";
 contract RPS {
     uint public numPlayer = 0;
     uint public reward = 0;
-    mapping (address => uint) public player_choice; // 0 - Scissors, 1 - Paper , 2 - Rock , 3 - Lizard , 4 - Spock : (x+1)%5, (x+3)%5
+    mapping(address => bytes32) private player_choice_commit;
+    mapping(address => uint) private player_choice;
     mapping(address => bool) public player_not_played;
     address[] public players;
     uint public numInput = 0;
+
     TimeUnit private timeUnit = new TimeUnit();
-    uint private timeOut = 1 ;
+    uint private timeOut = 1;
+    CommitReveal public commitReveal = new CommitReveal();
 
     address[4] private playerAccept = [
         0x5B38Da6a701c568545dCfcB03FcB875f56beddC4,
@@ -22,9 +25,9 @@ contract RPS {
         0x78731D3Ca6b7E34aC0F824c42a7cC18A495cabaB
     ];
 
-    function getValid () private view returns (bool) {
+    function getValid() private view returns (bool) {
         for (uint256 i = 0; i < 4; i++) {
-            if(msg.sender == playerAccept[i]) {
+            if (msg.sender == playerAccept[i]) {
                 return true;
             }
         }
@@ -35,22 +38,33 @@ contract RPS {
         require(numPlayer < 2);
         require(getValid());
         require(msg.value == 1 ether);
+
         reward += msg.value;
         player_not_played[msg.sender] = true;
         players.push(msg.sender);
         numPlayer++;
-        if(numPlayer==1){
+
+        if (numPlayer == 1) {
             timeUnit.setStartTime();
-        } 
+        }
     }
 
-    function input(uint choice) public {
+    function input(bytes32 choiceHash) public {
         require(numPlayer == 2);
         require(player_not_played[msg.sender]);
-        require(choice >= 0 && choice <= 4);
+        
+        uint choice = uint(choiceHash) % 5;
+        require(
+            choice == 0 || choice == 1 || choice == 2 || choice == 3 || choice == 4,
+            "Invalid choice"
+        );
+        
+        commitReveal.commit(choiceHash);
+        player_choice_commit[msg.sender] = choiceHash;
         player_choice[msg.sender] = choice;
         player_not_played[msg.sender] = false;
         numInput++;
+
         if (numInput == 2) {
             numInput = 0;
             numPlayer = 0;
@@ -63,40 +77,45 @@ contract RPS {
         uint p1Choice = player_choice[players[1]];
         address payable account0 = payable(players[0]);
         address payable account1 = payable(players[1]);
+
         if (p0Choice == p1Choice) {
             account0.transfer(reward / 2);
             account1.transfer(reward / 2);
+        } else if ((p1Choice + 1) % 5 == p0Choice || (p1Choice + 3) % 5 == p0Choice) {
+            account0.transfer(reward);
+        } else {
+            account1.transfer(reward);
         }
-        else if ((p1Choice + 1) % 5 == p0Choice || (p1Choice + 3) % 5 == p0Choice) {
-            account0.transfer(reward); // player 0 ชนะ 
-        }
-        else {
-            account1.transfer(reward); // player 1 ชนะ
-        }
+        
+        reward = 0;
+    }
+
+    function resetGame() private {
+        delete players;
+        numInput = 0;
+        numPlayer = 0;
         reward = 0;
     }
 
     function checkTimeOut() public {
-        if(timeUnit.elapsedMinutes() == timeOut) {
-            if(players.length == 1) {
+        if (timeUnit.elapsedMinutes() >= timeOut) {
+            if (players.length == 1) {
                 address payable account = payable(players[0]);
                 account.transfer(reward);
-                numInput = 0;
-                numPlayer = 0;
-                reward = 0;
+            } else if (numPlayer == 2 && numInput == 2) {
+                address payable account0 = payable(players[0]);
+                address payable account1 = payable(players[1]);
+                account0.transfer(reward / 2);
+                account1.transfer(reward / 2);
             } else {
                 for (uint256 i = 0; i < 2; i++) {
-                if(!player_not_played[players[i]]) {
-                    address payable account = payable(players[i]);
-                    account.transfer(reward);
-                    numInput = 0;
-                    numPlayer = 0;
-                    reward = 0;
+                    if (!player_not_played[players[i]]) {
+                        address payable account = payable(players[i]);
+                        account.transfer(reward);
+                    }
                 }
             }
-            }
-            
+            resetGame();
         }
     }
-
 }
